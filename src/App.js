0352d2b75404afdaca72bed0a5a87e7ea045277f
Sakthikannan.Subramanian <sakthikannan.subramanian@gmail.com>
2022-11-28 20:58:34 +0000

    Test push to see if issue is fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import './App.css';
-import Home from './containers/Home';
-import ChildrenContainer from './containers/ChildrenContainer';
-import FooterComponent from './components/Footer';
-import HeaderComponent from './components/Header';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Fab from '@mui/material/Fab';
-import { Link } from 'react-router-dom';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import {motion} from 'framer-motion';
+import "./App.css";
+import Home from "./containers/Home";
+import ChildrenContainer from "./containers/ChildrenContainer";
+import FooterComponent from "./components/Footer";
+import HeaderComponent from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Fab from "@mui/material/Fab";
+import { Link } from "react-router-dom";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import { motion } from "framer-motion";
 function App() {
-
-  const reloadHome = () =>{
+  const reloadHome = () => {
     window.location.reload();
-  }
+  };
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <HeaderComponent homedata="Quality Engineering Technology Stack" />
-        <div className='Container'>
+        <div className="Container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='home' element={<Home />} />
-            <Route path='testing' element={<ChildrenContainer />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="home" element={<Home />} />
+            {/* <Route path='testing' element={<ChildrenContainer />} /> */}
           </Routes>
         </div>
-        <motion.div style={{ marginLeft: '1%', cursor: 'move' }} drag dragConstraints={{ top: '10px', bottom: '30px' }}
+        <motion.div
+          style={{ marginLeft: "1%", cursor: "move" }}
+          drag
+          dragConstraints={{ top: "10px", bottom: "30px" }}
         >
           <Fab color="info" aria-label="add">
-            <Link to='/home' onClick={reloadHome}><HomeRoundedIcon/></Link>
+            <Link to="/home" onClick={reloadHome}>
+              <HomeRoundedIcon />
+            </Link>
           </Fab>
         </motion.div>
         <FooterComponent />
